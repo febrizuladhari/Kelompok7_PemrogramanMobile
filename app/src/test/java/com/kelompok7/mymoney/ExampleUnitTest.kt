4ad5a068b9c5e06d3898b7800bc35d335edbe3c0
Jo766hi <jonatansilalahi09@gmail.com>
2023-12-24 21:15:32 +0700

    Happy New Year

--- app/src/test/java/com/kelompok7/mymoney/ExampleUnitTest.kt
+++ app/src/test/java/com/kelompok7/mymoney/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.todolist
+package com.kelompok7.mymoney
 
 import org.junit.Test
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
@@ -10,8 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {url "https://jitpack.io"}
     }
 }
-rootProject.name = "To Do List"
-include ':app'
+
+rootProject.name = "MyMoney"
+include(":app")
+ 
